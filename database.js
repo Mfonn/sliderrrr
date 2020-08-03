444c5d28f3120130dc1f3c311cdc9028bf831ee5
Mfonn <67243684+Mfonn@users.noreply.github.com>
2020-08-03 23:03:20 +0100

    Update database.js

--- database.js
+++ database.js
@@ -40,3 +40,18 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+const spans = document.querySelectorAll('.word span');
+
+spans.forEach((span, idx) => {
+	span.addEventListener('click', (e) => {
+		e.target.classList.add('active');
+	});
+	span.addEventListener('animationend', (e) => {
+		e.target.classList.remove('active');
+	});
+	
+	// Initial animation
+	setTimeout(() => {
+		span.classList.add('active');
+	}, 750 * (idx+1))
+});
